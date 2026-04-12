1e0fa5e3b644f19ae91907255f04fc9d63d3267f
deutschich <danlu@gmx.at>
2026-04-12 16:26:26 +0200

    Initial commit

---
+++ src/main/java/com/user404_/ecoBack/EcoBack.java
@@ -0,0 +1,17 @@
+package com.user404_.ecoBack;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EcoBack extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
